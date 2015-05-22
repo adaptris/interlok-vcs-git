136ed5707ca8fabfc0d833877e77019f86e04d16
Sebastien Belin <sebastien.belin@adaptris.com>
2015-05-22 14:56:10 +0100

    INTERLOK-513 #comment Implement the testConnection method in the subversion and git modules. INTERLOK-513 #time 1h 45m The svn module only get the revision and the git module to a simple checkout

--- src/com/adaptris/vcs/git/api/JGitApi.java
+++ src/com/adaptris/vcs/git/api/JGitApi.java
@@ -25,34 +25,49 @@ import com.adaptris.core.management.vcs.VersionControlSystem;
 import com.adaptris.vcs.git.auth.AuthenticationProvider;
 
 public class JGitApi implements VersionControlSystem {
-  
+
   private static final String IMPL_NAME = "Git";
-  
+
   private static final String COPY_CLONE_POSTFIX = "_copy_doNotEdit";
-  
+
   private AuthenticationProvider authenticationProvider;
-  
+
   public JGitApi() {
   }
-  
+
   public JGitApi(AuthenticationProvider authenticationProvider) {
-    this.setAuthenticationProvider(authenticationProvider);
+    setAuthenticationProvider(authenticationProvider);
+  }
+
+  @Override
+  public String testConnection(String remoteRepoUrl, File workingCopyUrl) throws VcsException {
+    try {
+      CloneCommand cloneCommand = Git.cloneRepository().setURI(remoteRepoUrl).setDirectory(workingCopyUrl);
+      configureAuthentication(cloneCommand);
+      cloneCommand.setNoCheckout(true);
+      cloneCommand.call();
+    } catch (GitAPIException e) {
+      throw new VcsException(e);
+    }
+    // Maybe we should be able to return some sort of revision number
+    return null;
   }
 
   @Override
   public String checkout(String remoteRepoUrl, File workingCopyUrl) throws VcsException {
     // Now make a copy for our querying purposes.  We will also use this to update from.
     // This copy will always be up to date, regardless of the tag the user may have chosen.
-    this.checkoutCopy(remoteRepoUrl, workingCopyUrl);
-    
+    checkoutCopy(remoteRepoUrl, workingCopyUrl);
+
     try {
-      CloneCommand cloneCommand = Git.cloneRepository().setURI(this.getLocalCloneCopy(workingCopyUrl).getAbsolutePath()).setDirectory(workingCopyUrl);
+      CloneCommand cloneCommand = Git.cloneRepository().setURI(getLocalCloneCopy(workingCopyUrl).getAbsolutePath())
+          .setDirectory(workingCopyUrl);
       configureAuthentication(cloneCommand);
-      cloneCommand.call();  
+      cloneCommand.call();
     } catch (GitAPIException e) {
       throw new VcsException(e);
     }
-    return this.currentLocalRevision(workingCopyUrl);
+    return null;
   }
 
   @Override
@@ -63,44 +78,44 @@ public class JGitApi implements VersionControlSystem {
 
   @Override
   public String update(File workingCopyUrl, String tagName) throws VcsException {
-    this.updateCopy(workingCopyUrl); // get all branches and tags first in our copy
+    updateCopy(workingCopyUrl); // get all branches and tags first in our copy
     try {
-      CheckoutCommand checkoutCommand = this.getLocalRepository(workingCopyUrl).checkout().setName(tagName);
+      CheckoutCommand checkoutCommand = getLocalRepository(workingCopyUrl).checkout().setName(tagName);
       checkoutCommand.call();
     } catch (GitAPIException e) {
       throw new VcsException(e);
     }
-    return this.currentLocalRevision(workingCopyUrl);
+    return currentLocalRevision(workingCopyUrl);
   }
-  
+
 
   @Override
   public String update(File workingCopyUrl) throws VcsException {
-    this.updateCopy(workingCopyUrl); // get all branches and tags first in our copy
+    updateCopy(workingCopyUrl); // get all branches and tags first in our copy
     try {
-      Git localRepository = this.getLocalRepository(workingCopyUrl);
+      Git localRepository = getLocalRepository(workingCopyUrl);
       PullCommand pullCommand = localRepository.pull();
-      this.configureAuthentication(pullCommand);
+      configureAuthentication(pullCommand);
       pullCommand.call();
     } catch (GitAPIException e) {
       throw new VcsException(e);
     }
-    return this.currentLocalRevision(workingCopyUrl);
+    return currentLocalRevision(workingCopyUrl);
   }
 
   @Override
   public void commit(File workingCopyUrl, String commitMessage) throws VcsException {
     try {
-      Git localRepository = this.getLocalRepository(workingCopyUrl);
+      Git localRepository = getLocalRepository(workingCopyUrl);
       localRepository.commit().setAll(true).setMessage(commitMessage).call();
-      
+
       // This will push to our copy of the clone.
       PushCommand pushCommand = localRepository.push();
-      this.configureAuthentication(pushCommand);
+      configureAuthentication(pushCommand);
       pushCommand.call();
-      
+
       // Now push to the actual GIT server from our copy of the clone.
-      this.pushCopy(workingCopyUrl);
+      pushCopy(workingCopyUrl);
     } catch (GitAPIException e) {
       throw new VcsException(e);
     }
@@ -109,12 +124,12 @@ public class JGitApi implements VersionControlSystem {
   @Override
   public void recursiveAdd(File workingCopyUrl) throws VcsException {
     try {
-      this.getLocalRepository(workingCopyUrl).add().call();
+      getLocalRepository(workingCopyUrl).add().call();
     } catch (GitAPIException e) {
       throw new VcsException(e);
     }
   }
-  
+
   @Override
   public String getImplementationName() {
     return IMPL_NAME;
@@ -122,25 +137,25 @@ public class JGitApi implements VersionControlSystem {
 
   @Override
   public String getLocalRevision(File workingCopyUrl) throws VcsException {
-    return this.currentLocalRevision(workingCopyUrl);
+    return currentLocalRevision(workingCopyUrl);
   }
 
   @Override
   public String getRemoteRevision(String remoteRepoUrl, File workingCopyUrl) throws VcsException {
-    this.updateCopy(workingCopyUrl);    
-    return this.currentLocalRevision(this.getLocalCloneCopy(workingCopyUrl));
+    updateCopy(workingCopyUrl);
+    return currentLocalRevision(getLocalCloneCopy(workingCopyUrl));
   }
 
   @Override
   public List<RevisionHistoryItem> getRemoteRevisionHistory(String remoteRepoUrl, File workingCopyUrl, int limit) throws VcsException {
     List<RevisionHistoryItem> returnedsRevisions = new ArrayList<>();
-    this.updateCopy(workingCopyUrl); // get all commits etc into our copy, then querty that, without modifying the actual working copy.
-    
-    Git localRepositoryCopy = this.getLocalRepository(this.getLocalCloneCopy(workingCopyUrl));
+    updateCopy(workingCopyUrl); // get all commits etc into our copy, then querty that, without modifying the actual working copy.
+
+    Git localRepositoryCopy = getLocalRepository(getLocalCloneCopy(workingCopyUrl));
     try {
       ObjectId head = localRepositoryCopy.getRepository().resolve(Constants.HEAD);
       Iterable<RevCommit> commits = localRepositoryCopy.log().add(head).setMaxCount(limit).call();
-      
+
       for(RevCommit commit : commits) {
         RevisionHistoryItem item = new RevisionHistoryItem();
         item.setComment(commit.getFullMessage());
@@ -152,36 +167,38 @@ public class JGitApi implements VersionControlSystem {
     }
     return returnedsRevisions;
   }
-  
+
   @SuppressWarnings("rawtypes")
   private void configureAuthentication(TransportCommand command) {
-    if(this.getAuthenticationProvider() != null) {
-      if(this.getAuthenticationProvider().getCredentialsProvider() != null)
-        command.setCredentialsProvider(this.getAuthenticationProvider().getCredentialsProvider());
+    if(getAuthenticationProvider() != null) {
+      if(getAuthenticationProvider().getCredentialsProvider() != null) {
+        command.setCredentialsProvider(getAuthenticationProvider().getCredentialsProvider());
+      }
 
-      if(this.getAuthenticationProvider().getTransportInterceptor() != null)
-        command.setTransportConfigCallback(this.getAuthenticationProvider().getTransportInterceptor());
+      if(getAuthenticationProvider().getTransportInterceptor() != null) {
+        command.setTransportConfigCallback(getAuthenticationProvider().getTransportInterceptor());
+      }
     }
   }
-  
+
   private Git getLocalRepository(File localRepoDir) throws VcsException {
     FileRepositoryBuilder builder = new FileRepositoryBuilder();
     Repository repository;
     try {
       repository = builder
-        .setWorkTree(localRepoDir)
-        .readEnvironment()
-        .setup()
-        .build();
+          .setWorkTree(localRepoDir)
+          .readEnvironment()
+          .setup()
+          .build();
     } catch (IOException e) {
       throw new VcsException(e);
     }
-    
+
     return new Git(repository);
   }
-  
+
   private String currentLocalRevision(File workingCopyUrl) throws VcsException {
-    Git localRepository = this.getLocalRepository(workingCopyUrl);
+    Git localRepository = getLocalRepository(workingCopyUrl);
     ObjectId resolvedRevision;
     try {
       String fullBranch = localRepository.getRepository().getFullBranch();
@@ -191,10 +208,10 @@ public class JGitApi implements VersionControlSystem {
     }
     return resolvedRevision.getName();
   }
-  
+
   /**
    * This creates a copy of the local working repository.
-   * It's used to pull all tags, commits etc so we can query it without actually modifying the 
+   * It's used to pull all tags, commits etc so we can query it without actually modifying the
    * real working copy url.  We do it this way, because we cannot query the server side, we
    * have to keep a local working copy completely up to date.
    * @param remoteRepoUrl
@@ -203,39 +220,39 @@ public class JGitApi implements VersionControlSystem {
    */
   private void checkoutCopy(String remoteRepoUrl, File workingCopyUrl) throws VcsException {
     try {
-      CloneCommand cloneCommand = Git.cloneRepository().setURI(remoteRepoUrl).setDirectory(this.getLocalCloneCopy(workingCopyUrl));
+      CloneCommand cloneCommand = Git.cloneRepository().setURI(remoteRepoUrl).setDirectory(getLocalCloneCopy(workingCopyUrl));
       configureAuthentication(cloneCommand);
       cloneCommand.call();
     } catch (GitAPIException e) {
       throw new VcsException(e);
     }
   }
-  
+
   private void updateCopy(File workingCopyUrl) throws VcsException {
     try {
-      Git localRepository = this.getLocalRepository(this.getLocalCloneCopy(workingCopyUrl));
+      Git localRepository = getLocalRepository(getLocalCloneCopy(workingCopyUrl));
       PullCommand pullCommand = localRepository.pull();
-      this.configureAuthentication(pullCommand);
+      configureAuthentication(pullCommand);
       pullCommand.call();
     } catch (GitAPIException e) {
       throw new VcsException(e);
     }
   }
-  
+
   private File getLocalCloneCopy(File workingCopyFile) {
     File copyCloneFile = null;
     String workingCopyDirName = workingCopyFile.getName();
     File workingCopyFileParent = workingCopyFile.getParentFile();
-    
+
     copyCloneFile = new File(workingCopyFileParent, workingCopyDirName + COPY_CLONE_POSTFIX);
     return copyCloneFile;
   }
-  
+
   private void pushCopy(File workingCopyUrl) throws VcsException {
-    Git localRepositoryCopy = this.getLocalRepository(this.getLocalCloneCopy(workingCopyUrl));
+    Git localRepositoryCopy = getLocalRepository(getLocalCloneCopy(workingCopyUrl));
     // This will push our copy of the clone to the GIT server.
     PushCommand pushCommand = localRepositoryCopy.push();
-    this.configureAuthentication(pushCommand);
+    configureAuthentication(pushCommand);
     try {
       pushCommand.call();
     } catch (GitAPIException e) {
